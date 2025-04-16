258e84243d44e461b99ac5932b539d3e3d58344f
avlnkov <alex.lenkov@mail.ru>
2025-04-16 19:31:04 +0300

    внедрение зависимостей

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,9 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.media3.exoplayer)
+    implementation(libs.androidx.media3.exoplayer.dash)
+    implementation(libs.androidx.media3.ui)
     implementation(libs.coil.compose.v310)
     implementation(libs.coil.network.okhttp)
     implementation(libs.androidx.navigation.compose)
