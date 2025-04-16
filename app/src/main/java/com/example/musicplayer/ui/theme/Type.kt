0d2f10f7050ce5212a35b69028119f188341fdce
alex-and-dev <64987184+alex-android-dev@users.noreply.github.com>
2025-04-16 13:39:47 +0300

    Revert "gh-3: реализовал ViewModel, TrackListScreen, Mapper"

--- app/src/main/java/com/example/musicplayer/ui/theme/Type.kt
+++ app/src/main/java/com/example/musicplayer/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.musicplayer.presentation.theme
+package com.example.musicplayer.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
